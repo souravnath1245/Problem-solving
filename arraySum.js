774838ada765ae7890a42fde3d2b44314315f0cc
Sourav Nath <souravnath1245@gmail.com>
2022-04-03 02:30:33 +0600

    solving sum of array

---
+++ arraySum.js
@@ -0,0 +1,15 @@
+
+const  array = [ 1,2,3,4,7,8,9]
+
+
+// First Solution 
+let sumOfArray = 0;
+for(let i = 0 ; i < array.length ; i++){
+    sum += array[i]
+}
+console.log(sumOfArray)
+
+// Second Solution 
+let sum = array.reduce( (a, b) => a + b )
+console.log(sum)
+
